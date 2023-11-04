50b293bb96fd293801c403921216f9c7e4cec8ed
Gui-Sardinha <71841173+Guilherme-Sardinha@users.noreply.github.com>
2023-11-04 11:54:49 -0300

    Usando a DTO de usuario para trafegar dados para a resource

--- src/main/java/workshopMongo/resources/UserResource.java
+++ src/main/java/workshopMongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package workshopMongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import workshopMongo.DTO.UserDTO;
 import workshopMongo.domain.User;
 import workshopMongo.services.UserService;
 
@@ -19,12 +21,12 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-	
-		
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
+		List<UserDTO> listDTO = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
 		
-		return ResponseEntity.ok(list);
+		
+		return ResponseEntity.ok(listDTO);
 	}
 
 }
