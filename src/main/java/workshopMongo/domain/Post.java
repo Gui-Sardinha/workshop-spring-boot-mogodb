fa0de0d1c43b8de4dae06b9624e069104e254990
Gui-Sardinha <71841173+Guilherme-Sardinha@users.noreply.github.com>
2023-11-05 09:09:45 -0300

    colocando comentarios nos posts

--- src/main/java/workshopMongo/domain/Post.java
+++ src/main/java/workshopMongo/domain/Post.java
@@ -1,13 +1,16 @@
 package workshopMongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import workshopMongo.DTO.AuthorDTO;
+import workshopMongo.DTO.CommentDTO;
 
 
 @Document
@@ -22,6 +25,8 @@ public class Post implements Serializable{
 	private String body;
 	
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	
 	public Post() {
@@ -85,6 +90,17 @@ public class Post implements Serializable{
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 
 	@Override
 	public int hashCode() {
